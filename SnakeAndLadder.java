2bd943858874ea8fc482f3006b55378ecd39527e
Ehtasham <ehtashamhusain@gmail.com>
2021-03-12 08:37:58 +0530

    The playerss rolls the die to get number o to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,5 +2,8 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 	int Snake_Position = 0;
 	int Ladder_Position = 0;
+	double Snake = Math.floor(Math.random() * 10) % 7;
+	double Ladder = Math.floor(Math.random() * 10) % 7;
+	System.out.println(Snake+","+Ladder);
 	}
 }
