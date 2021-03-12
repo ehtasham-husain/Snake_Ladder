e960d59d20f757fd9dd0e8ee9d3920c3951f5aba
Ehtasham <ehtashamhusain@gmail.com>
2021-03-12 09:56:51 +0530

    The Player then checks for a Option

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,32 @@
 public class SnakeAndLadder {
+	public static final int Ladder = 0;
+	public static final int Snake = 1;
+
 	public static void main(String[] args) {
-	int Snake_Position = 0;
-	int Ladder_Position = 0;
-	double Snake = Math.floor(Math.random() * 10) % 7;
-	double Ladder = Math.floor(Math.random() * 10) % 7;
-	System.out.println(Snake+","+Ladder);
+	int Player_1_Position = 0;
+	int SnakeORLadder; 
+	//int Player_2_Position = 0;
+
+	int Player_1 = (int)(Math.random() * 10) % 7;
+	if(Player_1 == 0){
+	System.out.println(Player_1_Position);
+	}
+	else {
+	SnakeORLadder = (int)(Math.random() * 10) % 2;
+	System.out.println(SnakeORLadder);
+	System.out.println(Player_1);
+
+	switch(SnakeORLadder) {
+	case Ladder:
+	Player_1_Position += Player_1;
+	break;
+	case Snake:
+	Player_1_Position -= Player_1;
+	break;
+	default:
+	System.out.println(Player_1_Position);
+	}
+	}
+	System.out.println(Player_1_Position);
 	}
 }
